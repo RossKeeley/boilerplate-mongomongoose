0341c77738f21a1e5f8fcdcde0a248d890329704
Ross Keeley <rosskeeley@jennifers-mbp.home>
2020-11-19 20:14:42 +0000

    Configured mongodb and added to myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,16 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
